f0b95faae3873517186f71dd3e293c747387f1f0
morphicdhvanit <dhwanit@morphicitsolutions.com>
2026-02-24 19:19:57 +0530

    remove the 10 s delay

--- src/main/java/com/funfair/api/artists/ArtistsService.java
+++ src/main/java/com/funfair/api/artists/ArtistsService.java
@@ -47,11 +47,6 @@ public class ArtistsService {
 
 	public List<ArtistsDetailsDto> getAllArtistsDetails() {
 
-	    try {
-	        Thread.sleep(10000);
-	    } catch (InterruptedException e) {
-	        e.printStackTrace();
-	    }
 		List<ArtistsDetails> artistsDetails = artistsRespository.findAll();
 		List<ArtistsDetailsDto> artistsDetailsDtos = new ArrayList<>();
 		for (ArtistsDetails artistsDetail : artistsDetails) {
